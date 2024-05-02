5bbeffa405a3de38de59fc26e6ca1c694b14b0d5
Sumit katyal <skatyal1999@gmail.com>
2024-05-02 11:18:09 +0000

    create api

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -2,6 +2,13 @@ const router=require("express").Router()
 const Pin=require("../models/Pin")
 
 
-router.post("/",(req,res)=>{
+router.post("/",async(req,res)=>{
     const newPin=new Pin(req.body)
+    try{
+    const davepin=await newPin.save()
+    res.status(200).json(davepin)
+    }catch(err){
+        console.log(err)
+        res.status(500).json("error here",err)
+    }
 })
