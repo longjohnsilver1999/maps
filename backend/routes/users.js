3c0bb3a015c799fd11f8734d2b4ac375062b9d4b
sumitk-hub1999 <skatyal1999@gmail.com>
2024-05-03 07:39:55 +0530

    git

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,13 @@
-const router=require("express").Router()
-const User=require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
 
-
+router.post("/", async (req, res) => {
+  const newPin = new Pin(req.body);
+  try {
+    const davepin = await newPin.save();
+    res.status(200).json(davepin);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("error here", err);
+  }
+});
