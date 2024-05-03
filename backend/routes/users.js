13c6f44ba67a4255ac6718b2287fd1efbb83de29
sumitk-hub1999 <skatyal1999@gmail.com>
2024-05-03 09:41:29 +0530

    register user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 //register
 
 router.post("/register", async (req, res) => {
-  const newPin = new User(req.body);
+  //generate password create user and save user
   try {
-    const davepin = await newPin.save();
-    res.status(200).json(davepin);
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(req.body.password, salt);
+    const newPin = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashed,
+    });
+
+    const user = await newPin.save();
+
+    res.status(200).json(user);
   } catch (err) {
     console.log(err);
     res.status(500).json("error here", err);
